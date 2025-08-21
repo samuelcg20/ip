5263fd5ebbeca19a3bc024c1c84f8335a68a69fc
samuelcg20 <samuelchristygeorge@u.nus.edu>
2025-08-21 20:21:47 +0800

    Add support for Add List

--- src/main/java/Sai.java
+++ src/main/java/Sai.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Sai {
     private static final String NAME = "S.AI";
-    private final String GREETING_MESSAGE = "Hello! I'm %s \nWhat can I do for you?\n";
+    private final String GREETING_MESSAGE = "Hello! I'm %s \nWhat can I do for you?";
     private final String FAREWELL_MESSAGE = "Bye. Hope to see you again soon!";
+    private ArrayList<Task> list = new ArrayList<>();
+    private int last_pos = 0;
 
     private String wrap(String message) {
         return "-------------------------------------------------\n"
@@ -24,21 +27,91 @@ public class Sai {
         System.out.println(wrap(message));
     }
 
-    public void echo() {
+    public void chat() {
         Scanner scanner = new Scanner(System.in);
         this.greet();
         String input = scanner.nextLine();
 
         while (!input.equalsIgnoreCase("bye")) {
-            this.say(input);
-            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("list")) {
+                this.displayList();
+                input = scanner.nextLine();
+            } else if (input.toLowerCase().startsWith("mark")) {
+                this.mark(input);
+                input = scanner.nextLine();
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                this.unmark(input);
+                input = scanner.nextLine();
+            }
+            else {
+                this.addToList(input);
+                input = scanner.nextLine();
+            }
         }
 
         this.farewell();
     }
 
+    public void addToList(String listItem) {
+        this.list.add(new Task(listItem));
+        last_pos += 1;
+        this.say("added: " + listItem);
+    }
+
+    public void displayList() {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < last_pos; i++) {
+            if (i == last_pos - 1) {
+                output.append(String.format("%d. %s", i + 1, list.get(i)));
+            } else {
+                output.append(String.format("%d. %s\n", i + 1, list.get(i)));
+            }
+        }
+        System.out.println(wrap(output.toString()));
+    }
+
+    public void mark(String input) {
+        String[] splitInput = input.split(" ");
+        if (splitInput.length != 2) {
+            System.out.println(wrap("Please format your message as \"mark [task number]\""));
+        } else {
+            try {
+                int index = Integer.parseInt(splitInput[1]);
+                if (index <= last_pos) {
+                    Task item = this.list.get(index - 1);
+                    item.mark();
+                    System.out.println(wrap("Nice! I've marked this task as done:\n" + item));
+                } else {
+                    System.out.println(wrap("Task number does not exist"));
+                }
+            } catch (NumberFormatException e){
+                System.out.println(wrap("Please format your message as \"mark [task number]\""));
+            }
+        }
+    }
+
+    public void unmark(String input) {
+        String[] splitInput = input.split(" ");
+        if (splitInput.length != 2) {
+            System.out.println(wrap("Please format your message as \"unmark [task number]\""));
+        } else {
+            try {
+                int index = Integer.parseInt(splitInput[1]);
+                if (index <= last_pos) {
+                    Task item = this.list.get(index - 1);
+                    item.unmark();
+                    System.out.println(wrap("OK, I've marked this task as not done yet:\n" + item));
+                } else {
+                    System.out.println(wrap("Task number does not exist"));
+                }
+            } catch (NumberFormatException e){
+                System.out.println(wrap("Please format your message as \"unmark [task number]\""));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Sai mySai = new Sai();
-        mySai.echo();
+        mySai.chat();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,27 @@
 public class Task {
-    protected String description;
-    protected boolean isDone;
+    private final String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    public String getStatusIcon() {
+    private String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
     }
 
 }
