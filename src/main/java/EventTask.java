7a5f4fec2735941a29cfd89d50c444ed6ad7a58b
samuelcg20 <samuelchristygeorge@u.nus.edu>
2025-08-28 22:22:21 +0800

    Add support for teaching chatbot how to understand dates and times

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,23 +1,29 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class EventTask extends Task{
-    private String start;
-    private String end;
+    private LocalDateTime start;
+    private LocalDateTime end;
 
     public EventTask(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        this.start = DateTimeParser.parseDateTime(start);
+        this.end = DateTimeParser.parseDateTime(end);
     }
 
-    public String getStart() {
+    public LocalDateTime getStart() {
         return start;
     }
 
-    public String getEnd() {
+    public LocalDateTime getEnd() {
         return end;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+        return "[E]" + super.toString()
+                + " (from: " + start.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma"))
+                + " to: " + end.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma")) + ")";
     }
 }
